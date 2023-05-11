3d2c6f0fe037d4d1f2c6d8647f136776ad747d15
User <User@WIN-SUEO6K7NQMN>
2023-05-11 11:31:05 +0400

    Add project files.

---
+++ StudentTask/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var students = new List<Student>
+            {
+                new Student{
+                    Id=1,
+                    Name="Aykhan"
+                },
+                new Student{
+                    Id=2,
+                    Name="Elvin"
+                },
+                new Student{
+                    Id=3,
+                    Name="Sahlar"
+                }
+            };
+
+            return View(students);
+        }
+    }
+}
